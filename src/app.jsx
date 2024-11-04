198ca39fdbeef498a08369d232c20309e13c2459
Gage Smith <gagesmith242@gmail.com>
2024-11-04 14:31:48 -0700

    Navigation added

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,45 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
 import './app.css';
 
 export default function App() {
   return (
-    <div className='body'>
-      <header>
-        <h1>Orbit Game</h1>
-        <nav>
-          <menu>
-            <a href="index.html">Home</a>
-            <a href="play.html">Play</a>
-            <a href="scores.html">Leaderboard</a>
-            <a href="about.html">About</a>
-          </menu>
-        </nav>
-      </header>
+    <BrowserRouter>
+      <div className='body'>
+        <header>
+          <h1>Orbit Game</h1>
+          <nav>
+            <menu>
+              <NavLink className='nav-link' to=''>Login</NavLink>
+              <NavLink className='nav-link' to='play'>Play</NavLink>
+              <NavLink className='nav-link' to='scores'>Leaderboard</NavLink>
+              <NavLink className='nav-link' to='about'>About</NavLink>
+            </menu>
+          </nav>
+        </header>
 
-      <main>App components go here</main>
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route path='/play' element={<Play />} />
+          <Route path='/scores' element={<Scores />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-      <footer>
-        <span>Made by Gage Smith</span>
-        <br />
-        <a href="https://github.com/12gagesmith/orbit_game">GitHub Repository</a>
-      </footer>
-    </div>
+        <footer>
+          <span>Made by Gage Smith</span>
+          <br />
+          <a href="https://github.com/12gagesmith/orbit_game">GitHub Repository</a>
+        </footer>
+      </div>
+    </BrowserRouter>
   )
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
