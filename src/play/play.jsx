c8dbf6efb16a1d674fb2fc5803db2748d7a9d792
Gage Smith <gagesmith242@gmail.com>
2024-11-09 12:27:27 -0700

    Saving progress on orbit function

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -17,36 +17,124 @@ export function Play() {
 
   const [squares, setSquares] = useState(["public/slot_down_empty.png", "public/slot_left_empty.png", "public/slot_left_empty.png", "public/slot_left_empty.png", "public/slot_down_empty.png", "public/slot_down_empty.png", "public/slot_left_empty.png", "public/slot_up_empty.png", "public/slot_down_empty.png", "public/slot_right_empty.png", "public/slot_up_empty.png", "public/slot_up_empty.png", "public/slot_right_empty.png", "public/slot_right_empty.png", "public/slot_right_empty.png", "public/slot_up_empty.png"]);
   const [whiteIsNext, setWhiteIsNext] = useState(true);
+
+  function orbit(currSquares) {
+    const temp1 = currSquares[0]
+    const temp2 = currSquares[5]
+    if (currSquares[1] == "public/slot_left_white") {
+      currSquares[0] = "public/slot_down_white"
+    } else if (currSquares[1] == "public/slot_left_blue") {
+      currSquares[0] = "public/slot_down_blue"
+    }
+    if (currSquares[2] == "public/slot_left_white") {
+      currSquares[1] = "public/slot_left_white"
+    } else if (currSquares[2] == "public/slot_left_blue") {
+      currSquares[1] = "public/slot_left_blue"
+    }
+    if (currSquares[3] == "public/slot_left_white") {
+      currSquares[2] = "public/slot_left_white"
+    } else if (currSquares[3] == "public/slot_left_blue") {
+      currSquares[2] = "public/slot_left_blue"
+    }
+    if (currSquares[7] == "public/slot_up_white") {
+      currSquares[3] = "public/slot_left_white"
+    } else if (currSquares[7] == "public/slot_up_blue") {
+      currSquares[3] = "public/slot_left_blue"
+    }
+    if (currSquares[11] == "public/slot_up_white") {
+      currSquares[7] = "public/slot_up_white"
+    } else if (currSquares[11] == "public/slot_up_blue") {
+      currSquares[7] = "public/slot_up_blue"
+    }
+    if (currSquares[15] == "public/slot_up_white") {
+      currSquares[11] = "public/slot_up_white"
+    } else if (currSquares[15] == "public/slot_up_blue") {
+      currSquares[11] = "public/slot_up_blue"
+    }
+    if (currSquares[14] == "public/slot_right_white") {
+      currSquares[15] = "public/slot_up_white"
+    } else if (currSquares[14] == "public/slot_right_blue") {
+      currSquares[15] = "public/slot_up_blue"
+    }
+    if (currSquares[13] == "public/slot_right_white") {
+      currSquares[14] = "public/slot_right_white"
+    } else if (currSquares[13] == "public/slot_right_blue") {
+      currSquares[14] = "public/slot_right_blue"
+    }
+    if (currSquares[12] == "public/slot_right_white") {
+      currSquares[13] = "public/slot_right_white"
+    } else if (currSquares[12] == "public/slot_right_blue") {
+      currSquares[13] = "public/slot_right_blue"
+    }
+    if (currSquares[8] == "public/slot_down_white") {
+      currSquares[12] = "public/slot_right_white"
+    } else if (currSquares[8] == "public/slot_down_blue") {
+      currSquares[12] = "public/slot_right_blue"
+    }
+    if (currSquares[4] == "public/slot_down_white") {
+      currSquares[8] = "public/slot_down_white"
+    } else if (currSquares[4] == "public/slot_down_blue") {
+      currSquares[8] = "public/slot_down_blue"
+    }
+    if (temp1 == "public/slot_down_white") {
+      currSquares[4] = "public/slot_down_white"
+    } else if (temp1 == "public/slot_down_blue") {
+      currSquares[4] = "public/slot_down_blue"
+    }
+    if (currSquares[6] == "public/slot_left_white") {
+      currSquares[5] = "public/slot_down_white"
+    } else if (currSquares[6] == "public/slot_left_blue") {
+      currSquares[5] = "public/slot_down_blue"
+    }
+    if (currSquares[10] == "public/slot_up_white") {
+      currSquares[6] = "public/slot_left_white"
+    } else if (currSquares[10] == "public/slot_up_blue") {
+      currSquares[6] = "public/slot_left_blue"
+    }
+    if (currSquares[9] == "public/slot_right_white") {
+      currSquares[10] = "public/slot_up_white"
+    } else if (currSquares[9] == "public/slot_right_blue") {
+      currSquares[10] = "public/slot_up_blue"
+    }
+    if (temp2 == "public/slot_down_white") {
+      currSquares[9] = "public/slot_right_white"
+    } else if (temp2 == "public/slot_down_blue") {
+      currSquares[9] = "public/slot_right_blue"
+    }
+    return currSquares;
+  }
   
   function handleSquareClick(i) {
     const nextSquares = squares.slice();
-    if (i == 0 || i == 4 || i == 5 || i == 8) {
-      if (whiteIsNext) {
-        nextSquares[i] = "public/slot_down_white.png";
+    if (nextSquares[i] == "public/slot_down_empty.png" || nextSquares[i] == "public/slot_left_empty.png" || nextSquares[i] == "public/slot_up_empty.png" || nextSquares[i] == "public/slot_right_empty.png") {
+      if (i == 0 || i == 4 || i == 5 || i == 8) {
+        if (whiteIsNext) {
+          nextSquares[i] = "public/slot_down_white.png";
+        } else {
+          nextSquares[i] = "public/slot_down_blue.png";
+        }
+      } else if (i == 1 || i == 2 || i == 3 || i == 6) {
+        if (whiteIsNext) {
+          nextSquares[i] = "public/slot_left_white.png";
+        } else {
+          nextSquares[i] = "public/slot_left_blue.png";
+        }
+      } else if (i == 7 || i == 10 || i == 11 || i == 15) {
+        if (whiteIsNext) {
+          nextSquares[i] = "public/slot_up_white.png";
+        } else {
+          nextSquares[i] = "public/slot_up_blue.png";
+        }
       } else {
-        nextSquares[i] = "public/slot_down_blue.png";
+        if (whiteIsNext) {
+          nextSquares[i] = "public/slot_right_white.png";
+        } else {
+          nextSquares[i] = "public/slot_right_blue.png";
+        }
       }
-    } else if (i == 1 || i == 2 || i == 3 || i == 6) {
-      if (whiteIsNext) {
-        nextSquares[i] = "public/slot_left_white.png";
-      } else {
-        nextSquares[i] = "public/slot_left_blue.png";
-      }
-    } else if (i == 7 || i == 10 || i == 11 || i == 15) {
-      if (whiteIsNext) {
-        nextSquares[i] = "public/slot_up_white.png";
-      } else {
-        nextSquares[i] = "public/slot_up_blue.png";
-      }
-    } else {
-      if (whiteIsNext) {
-        nextSquares[i] = "public/slot_right_white.png";
-      } else {
-        nextSquares[i] = "public/slot_right_blue.png";
-      }
+      setSquares(orbit(nextSquares))
+      setWhiteIsNext(!whiteIsNext)
     }
-    setSquares(nextSquares)
-    setWhiteIsNext(!whiteIsNext)
   }
 
   return (
